04051a66031f844a08a489f4c5b08a6db7b9abd5
exchavez09 <76718764+exchavez09@users.noreply.github.com>
2021-07-07 20:26:08 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+noseX=0;
+noseY=0;
+
+
+function preload(){
+    clown_nose = loadImage('https://i.postimg.cc/66nkYzg8/m.png');
+
+}
+
+function setup(){
+    canvas = createCanvas(400,400);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(400, 400);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+    }
+}
+
+function draw(){
+    image(video, 0, 0, 400, 400);
+    fill(255,0,0);
+    stroke(255,0,0);
+    image(clown_nose, noseX, noseY, 30, 30);
+}
+
+function take_snapshot(){
+    save('Img1.png');
+}
